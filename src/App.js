3e956602636dd14a7f83e20430c4caf70bd88fb4
Aleksey <dev.zhuravlev@gmail.com>
2020-08-28 21:45:23 +0300

    'initial'

---
+++ src/App.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import {createStore} from "redux";
+import { Provider } from 'react-redux'
+import {BrowserRouter, Switch, Route} from "react-router-dom";
+
+import Main from "./components/Main/Main.js";
+import mainReducer from "./reduxStore";
+
+import "./App.scss";
+
+const store = createStore(
+    mainReducer,
+    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+    );
+
+export default class App extends React.Component {
+
+  render() {
+    return (
+      <Provider store={store}>
+          <BrowserRouter>
+              <Switch>
+                  <Route exac path={"/main"} component={Main}/>
+              </Switch>
+          </BrowserRouter>
+      </Provider>
+    );
+  }
+};
